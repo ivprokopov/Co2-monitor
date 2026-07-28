161b433fa21e42d00c28b7f08a75d0d3f733cab3
Ivailo Prokopov <iprokopov@gmail.com>
2026-07-28 09:56:06 +0300

    Add weather coordinates and location name to config

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 window.APP_CONFIG = {
-  FIREBASE_URL: "https://rfid-project-e68a3-default-rtdb.europe-west1.firebasedatabase.app"
+  FIREBASE_URL: "https://rfid-project-e68a3-default-rtdb.europe-west1.firebasedatabase.app",
+  WEATHER_LAT: 42.931793,
+  WEATHER_LON: 23.202095,
+  LOCATION_NAME: "с. Дръмша"
 };
